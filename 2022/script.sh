a35770af4761d621110fc00546990144cdb740e8
omagdy7 <omar.professional8777@gmail.com>
2022-12-03 13:58:29 +0200

    Fixed something in the Makefile

--- 2022/script.sh
+++ 2022/script.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
+read N
 
-for i in {3..25}
-do
-  echo "d" | ~/.scripts/cpp_ps.sh "Day$i"
-  cp Makefile Day$i
-  touch "Day$i"/input.txt
-done
+echo "d" | ~/.scripts/cpp_ps.sh "Day$N"
+cp Makefile Day$N
+touch "Day$N"/input.txt
 
